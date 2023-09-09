0bea9ca541ba7f545b722f41d540c905980d83bb
toil-exam <141971431+toil-exam@users.noreply.github.com>
2023-09-09 17:36:50 -0400

    Update Matrix.lua

--- Matrix.lua
+++ Matrix.lua
@@ -1,7 +1,5 @@
 Matrix = {}
 
-
-
 function Matrix.__init__()
     local self = {}
 
@@ -11,10 +9,38 @@ function Matrix.__init__()
         :setBackground(colors.black)
         :setBorder(colors.green)
 
+    -- idk maybe pass this somewhere
     self.pad = 3
 
+    -- derive x and y padding
     self.padX = self.pad * 2
     self.padY = self.pad
+
+    -- pull monitor size and adjust per padding
+    self.x, self.y = self.monitor:getSize()
+
+    self.x = self.x - (self.padX * 2)
+    self.y = self.y - (self.padY * 2)
+
+
+    self.text = {}
+
+    for x = 1, self.x do
+        self.text[x] = {}
+        for y = 1, self.y do
+            self.text[x][y] = self.monitor:addTextField()
+                :setForeground(colors.green)
+                :setSize(1, 1)
+                :setPosition()
+                :addLine("x")
+        end
+    end
+
+
+    --self.text = self.monitor:addTextField()
+    --    :setForeground(colors.green)
+    --    :setSize(self.x, self.y)
+    --    :setPosition(self.padX + 1, self.padY + 1)
 
     self.thread = self.monitor:addThread()
         :start(Matrix.autoUpdate(self))
@@ -28,6 +54,7 @@ function Matrix.autoUpdate(self)
     self.basalt.autoUpdate()
 end
 
-local matrix = Matrix.__init__()
+setmetatable(Matrix, {__call = Matrix.__init__})
 
---matrix.autoUpdate(matrix)
+local matrix = Matrix()
+
