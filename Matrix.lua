168f8a2661a94bec4533f2396828c294fc8cadbe
toil-exam <141971431+toil-exam@users.noreply.github.com>
2023-09-09 16:48:03 -0400

    Create Matrix.lua

---
+++ Matrix.lua
@@ -0,0 +1,15 @@
+Matrix = {}
+
+basalt = require("basalt")
+monitor = basalt.addMonitor():setMonitor("left")
+
+
+function Matrix.__init__()
+    local self = {}
+    setmetatable(self, {__index = Matrix})
+    return self
+end
+
+local aThred = monitor:addThread():start(Matrix.__init__)
+
+basalt.autoUpdate()
