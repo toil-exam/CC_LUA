560c5a5325ed3a4e290107983beeaee39403114f
toil-exam <141971431+toil-exam@users.noreply.github.com>
2023-09-09 19:33:57 -0400

    Update Matrix.lua

--- Matrix.lua
+++ Matrix.lua
@@ -1,4 +1,5 @@
 Matrix = {}
+Drips = {}
 
 local basalt = require("basalt")
 local monitor = basalt.addMonitor()
@@ -18,19 +19,36 @@ y = y - (padY * 2)
 
 for a = 1, x do
     Matrix[a] = {}
+    Drips[a] = -1
     for b = 1, y do
         Matrix[a][b] = monitor:addTextfield()
             :setForeground(colors.green)
             :setSize(1, 1)
-            :setPosition(a + padX, y - b + padY) -- negation on y axis to make origin bottom left
-            :addLine("?", 1)
+            :setPosition(a + padX, y - b + padY + 1) -- negation on y axis to make origin bottom left
+            :addLine(" ", 1)
     end
 end
 
 function Run()
     while true do
-        Matrix[math.random(x)][math.random(y)]
-            :editLine(1, string.char(math.random(128)))
+        -- spawn new drip
+        Drips[math.random(x)] = 1
+
+        -- iterate through drips
+        for d = 1, x do
+            if Drips[d] ~= 0 then
+                Drips[d] = Drips[d] + 1
+            end
+
+            if Drips[d] > y then
+                Drips[d] = 0
+            else
+                Matrix[d][Drips[d]]:editLine(1, string.char(math.random(0,128)))
+            end
+        end
+
+        --Matrix[math.random(x)][math.random(y)]:editLine(1, string.char(math.random(128)))
+        
         os.sleep(1)
     end
 end
