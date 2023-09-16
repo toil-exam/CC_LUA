fcbc2619f40ac8cb5be82ca9976b13dad36435da
toil-exam <141971431+toil-exam@users.noreply.github.com>
2023-09-16 18:14:18 -0400

    Create 2048.lua

---
+++ 2048.lua
@@ -0,0 +1,60 @@
+Values = {}
+Texts = {}
+Buttons = {}
+
+local basalt = require("basalt")
+local monitor = basalt.addMonitor()
+    :setMonitor("left")
+    :setBackground(colors.black)
+    :setBorder(colors.blue)
+
+
+local pad = 3
+
+local padX = pad * 2
+local padY = pad
+
+local x, y = monitor:getSize()
+
+x = x - (padX * 2)
+y = y - (padY * 2)
+
+for row = 1, 4 do
+    Values[row] = {}
+    Texts[row] = {}
+    for col = 1, 4 do
+        Values[row][col] = 0
+        Texts[row][col] = monitor:addTextfield()
+            :setForeground(colors.white)
+            :setSize(1, 1)
+            :setPosition(row + padX, y - col + padY + 1) -- negation on y axis to make origin bottom left
+            :addLine("0", 1)
+    end
+end
+
+-- "text", x offset, y offset
+local button_init = {
+    {"up", 0, -1},
+    {"right", 1, 0},
+    {"down", 0, 1},
+    {"left", -1, 0},
+}
+
+for i = 1, 4 do
+    local text = button_init[i][1]
+    local x_offset = button_init[i][2]
+    local y_offset = button_init[i][3]
+
+    --Buttons.text = monitor:
+end
+
+function Run()
+    while true do
+        
+        os.sleep(1)
+    end
+end
+
+
+
+basalt.autoUpdate()
